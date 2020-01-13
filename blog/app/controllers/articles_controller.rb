39d483c12fb13fac0dbdc6f7301448b2d0b1b6e2
Kevin Gorham <kevin.gorham@gmail.com>
2020-01-13 14:31:01 -0800

    added update functionality and links

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -7,8 +7,17 @@ class ArticlesController < ApplicationController
       @article = Article.new
    end
 
+   def edit
+      @article = Article.find(params[:id])
+   end
+
+   def show
+      @article = Article.find(params[:id])
+   end 
+
    def create
       @article = Article.new(article_params)
+      
       if @article.save
          redirect_to @article
       else
@@ -16,8 +25,14 @@ class ArticlesController < ApplicationController
       end
    end
 
-   def show
+   def update
       @article = Article.find(params[:id])
+
+      if @article.update(article_params)
+         redirect_to @article
+      else
+         render 'edit'
+      end
    end
 
    private
