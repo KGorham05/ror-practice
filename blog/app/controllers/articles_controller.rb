de6df14683aa34e26aa9a64bd9fc2e10a2117969
Kevin Gorham <kevin.gorham@gmail.com>
2020-01-13 14:57:59 -0800

    Deleting articles functionality and links added

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -2,7 +2,11 @@ class ArticlesController < ApplicationController
    def index 
       @articles = Article.all
    end
-
+   
+   def show
+      @article = Article.find(params[:id])
+   end 
+   
    def new
       @article = Article.new
    end
@@ -10,10 +14,6 @@ class ArticlesController < ApplicationController
    def edit
       @article = Article.find(params[:id])
    end
-
-   def show
-      @article = Article.find(params[:id])
-   end 
 
    def create
       @article = Article.new(article_params)
@@ -33,6 +33,13 @@ class ArticlesController < ApplicationController
       else
          render 'edit'
       end
+   end
+
+   def destroy
+      @article = Article.find(params[:id])
+      @article.destroy
+
+      redirect_to articles_path
    end
 
    private
